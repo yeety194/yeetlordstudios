ea5060b561b1a2f27c2a9cc3ad5269caeb2a5293
yeetlord1590 <102434608+yeety194@users.noreply.github.com>
2025-05-10 20:26:36 -0400

    small change

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+// Base JavaScript Script
+
+// Function to log a message to the console
+function logMessage(message) {
+    console.log(message);
+}
+
+// Example usage
+logMessage("Hello, world!");
+
+// Add your custom functions and logic below
