70cc6a1d2978c98c5620c13306baa7df3590ac4b
yeetlord1590 <102434608+yeety194@users.noreply.github.com>
2025-05-21 21:10:14 -0400

    Changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # yeetlordstudios
 
-# This is a side project I have been working on for the past 3 months
+This is a side project I have been working on for the past 3 months
--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 // script.js
 // Base JavaScript Script
 
-// Function to log a message to the console
-function logMessage(message) {
-    console.log(message);
-}
-
-// Example usage
-logMessage("Hello, world!");
-
-// Add your custom functions and logic below
+// Davids Work
